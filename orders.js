4a7cb64d7c65fd5a68de4ac391ebafdb8076899c
B Manoj Kumar Reddy <Manoj.Reddy@obloliving.com>
2023-07-12 09:48:25 +0530

    first commit

---
+++ orders.js
@@ -0,0 +1,38 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '87531',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Digi Drone',
+        productNumber: '55331',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Drone with GPS',
+        productNumber: '45291',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'FRuko F11 Pro Drone',
+        productNumber: '87531',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '87531',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '87531',
+        paymentStatus: 'Paid',
+        shipping: 'Pending'
+    },
+]
